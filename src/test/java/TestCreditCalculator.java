3c2944dd51b5232753474d04e41f265c15b3c431
nik86olay <nik86olay@gmail.com>
2022-08-02 21:10:57 +0300

    Add TestClass commit

--- src/test/java/TestCreditCalculator.java
+++ src/test/java/TestCreditCalculator.java
@@ -1,47 +1,51 @@
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestCreditCalculator {
 
-    final static double EXPECTED_CREDIT_RATE = 10;
+    static final double EXPECTED_CREDIT_RATE = 10;
 
     @ParameterizedTest
-    @CsvSource({"0, 100 000, 2 000 000",
-            "3, 2 000 000, 100 000",
-            "1, 100 000, 2 000 000",
-            "35, 500 000, 2 500 000"})
+    @CsvSource({"0, 100 000, 2 000 000, 1",
+            "3, 2 000 000, 100 000, один",
+            "1, 100 000, 2 000 000, 2",
+            "35, 500 000, 2 500 000, 3"})
     @DisplayName("Тест работы dataVerification по проверке валидности входных параметров")
-    public void test_validParam_method(int month, int sumObject, int firstSum) {
+    public void test_validParam_method(int month, int sumObject, int firstSum, String operation) {
         // given:
         CreditCalculator calculator = new CreditCalculator();
-        boolean expected = month == 0 || month > 30 || sumObject < firstSum;
+        boolean expected = month == 0 || month > 30 || sumObject < firstSum || operation.equals("один");
 
         // when:
-        var actual = calculator.dataVerification(month, sumObject, firstSum);
+        var actual = calculator.dataVerification(month, sumObject, firstSum, operation);
 
         // then:
         // Проверка результата работы метода
-        assertThat(CreditCalculator.CreditRate, equals(EXPECTED_CREDIT_RATE));
+        assertTrue(CreditCalculator.CREDIT_RATE == EXPECTED_CREDIT_RATE);
         assertThat(actual, equals(expected));
 
     }
 
     @ParameterizedTest
-    @CsvSource({"1, 100 000, 2 000 000, 174166.66",
-            "30, 550 000, 2 000 000, 16 111.11"})
-    @DisplayName("Тест работы calculate по вычислению ежемесячного платежа")
-    public void test_validResult_method(int month, int sumObject, int firstSum, int expected) {
+    @CsvSource({"1, 100_000, 2_000_000, 2_090_000, 2",
+            "30, 550_000, 2_000_000, 16_111.11, 1",
+            "15, 550_000, 2_000_000, 3_625_000, 3"})
+    @DisplayName("Тест работы calculateMonthPay по вычислению ежемесячного платежа")
+    public void test_validResult_method(int month, int sumObject, int firstSum, String operation, int expected) {
         // given:
         CreditCalculator calculator = new CreditCalculator();
 
         // when:
-        var result = calculator.calculate(month, sumObject, firstSum);
+        var result = calculator.calculate(month, sumObject, firstSum, operation);
 
         // then:
         // Проверка результата работы метода
         assertThat(result, equals(expected));
     }
+
 }
